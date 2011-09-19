a79318957713544763ab3a6eefb8e7c8f522d846
Ludwig Schwardt <ludwig@ska.ac.za>
2011-09-19 12:12:46 +0200

    Initial commit of new scikits.fitting package. Put fitting.py and its tests into the scikits structure, added Modified BSD License and produced an initial setup.py scrounged from scikits.image.

---
+++ README.txt
@@ -0,0 +1,33 @@
+Fitting SciKit
+==============
+
+A framework for fitting functions to data with SciPy which unifies the various
+available interpolation methods and provides a common interface to them.
+
+Source
+------
+https://github.com/ludwigschwardt/scikits.fitting
+
+Installation from source
+------------------------
+
+The SciKit may be installed globally using
+
+python setup.py install
+
+or locally using
+
+python setup.py install --prefix=${HOME}
+
+If you prefer, you can use it without installing, by simply adding
+this path to your PYTHONPATH variable and compiling the extensions::
+
+  python setup.py build_ext -i
+
+License
+-------
+Please read LICENSE.txt in this directory.
+
+Contact
+-------
+Ludwig Schwardt <ludwig at ska.ac.za>
---
+++ setup.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+
+descr   = """Fitting SciKit.
+
+A framework for fitting functions to data with SciPy which unifies the various
+available interpolation methods and provides a common interface to them.
+"""
+
+DISTNAME            = 'scikits.fitting'
+DESCRIPTION         = 'Framework for fitting functions to data with SciPy'
+LONG_DESCRIPTION    = descr
+MAINTAINER          = 'Ludwig Schwardt'
+MAINTAINER_EMAIL    = 'ludwig@ska.ac.za'
+URL                 = 'https://github.com/ludwigschwardt/scikits.fitting'
+LICENSE             = 'Modified BSD'
+DOWNLOAD_URL        = 'https://github.com/ludwigschwardt/scikits.fitting'
+VERSION             = '0.5'
+
+import os
+import setuptools
+from numpy.distutils.core import setup
+
+def configuration(parent_package='', top_path=None):
+    if os.path.exists('MANIFEST'): os.remove('MANIFEST')
+
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(None, parent_package, top_path,
+                           namespace_packages=['scikits'])
+
+    config.set_options(
+            ignore_setup_xxx_py=True,
+            assume_default_configuration=True,
+            delegate_options_to_subpackages=True,
+            quiet=True)
+
+    config.add_subpackage('scikits')
+    config.add_subpackage(DISTNAME)
+    config.add_data_files('scikits/__init__.py')
+
+    return config
+
+if __name__ == "__main__":
+
+    setup(
+        name=DISTNAME,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        url=URL,
+        license=LICENSE,
+        download_url=DOWNLOAD_URL,
+        version=VERSION,
+
+        classifiers=['Development Status :: 4 - Beta',
+                     'Environment :: Console',
+                     'Intended Audience :: Developers',
+                     'Intended Audience :: Science/Research',
+                     'License :: OSI Approved :: BSD License',
+                     'Topic :: Scientific/Engineering'],
+
+        configuration=configuration,
+        install_requires=[],
+        namespace_packages=['scikits'],
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        zip_safe=True, # the package can run out of an .egg file
+        #test_suite="tester", # for python setup.py test
+        )
